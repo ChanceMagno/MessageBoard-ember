beedbd27b8389b11758a9299e3b501375aed12ff
Chance <ChanceMagno@gmail.com>
2017-04-28 12:06:44 -0700

    add save backend methods

--- app/components/add-post.js
+++ app/components/add-post.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    savePost() {
+      var params = {
+        name: this.get('name'),
+        img: this.get('img'),
+        question: this.get('question'),
+        content: this.get('content')
+      };
+
+      this.sendAction('savePost', params);
+    }
+  }
 });
