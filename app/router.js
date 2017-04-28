7032f858adf1d62b203a247f3e146b8155bb9144
Chance <ChanceMagno@gmail.com>
2017-04-28 12:46:30 -0700

    add view single post component, add link to main page, add route to view page, add css styling

--- app/router.js
+++ app/router.js
@@ -10,6 +10,7 @@ Router.map(function() {
   this.route('about');
   this.route('contact');
   this.route('add-post');
+  this.route('view-post', {path: '/view-post/:post_id'});
 });
 
 export default Router;
